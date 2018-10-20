1b100483660be3ad4c51dd113a540388845ebc93
anolan19 <anolan19@terpmail.umd.edu>
2018-10-19 21:45:41 -0400

    fixing the thing

--- project/app/src/main/java/com/example/a/ycphack2018/account.java
+++ project/app/src/main/java/com/example/a/ycphack2018/account.java
@@ -1,4 +1,5 @@
 package com.example.a.ycphack2018;
 
 public class account {
+
 }
--- project/app/src/main/java/com/example/a/ycphack2018/listing.java
+++ project/app/src/main/java/com/example/a/ycphack2018/listing.java
@@ -1,4 +1,16 @@
 package com.example.a.ycphack2018;
 
+import java.util.ArrayList;
+
 public class listing {
+    private account lister;
+    private ArrayList<account> contributers;
+    private String description;
+    private String shortdes;
+    private ArrayList<String> ideaCat;
+    private int score;
+    private int outOf;
+    private boolean isPrivate;
+
+
 }
