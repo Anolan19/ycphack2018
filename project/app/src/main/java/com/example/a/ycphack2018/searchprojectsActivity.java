a632afe4354f5b6da14e80e8ea33e9931acc083a
anolan19 <anolan19@terpmail.umd.edu>
2018-10-20 16:59:14 -0400

    procedurial add

--- project/app/src/main/java/com/example/a/ycphack2018/searchprojectsActivity.java
+++ project/app/src/main/java/com/example/a/ycphack2018/searchprojectsActivity.java
@@ -2,6 +2,7 @@ package com.example.a.ycphack2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -9,27 +10,32 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.function.Consumer;
 
-public class searchprojects extends AppCompatActivity {
+public class searchprojectsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchprojects);
-        ArrayList<String> listofEvents;
+        final ArrayList<account> listofEvents= new ArrayList<>();
 
         FirebaseDatabase.getInstance().getReference().child("listing").orderByChild("score").addValueEventListener(new ValueEventListener(){
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot child: dataSnapshot.getChildren()){
-
+                Iterator<DataSnapshot> children = dataSnapshot.getChildren().iterator();
+                while(children.hasNext()&&listofEvents.size()<30){
+                    DataSnapshot childSnapshot = (DataSnapshot) children.next();
+                    account a = (account) childSnapshot.getValue();
+                    listofEvents.add(a);
                 }
+
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+            Log.i("DATABASECONNERR",databaseError.toString());
             }
         });
 
