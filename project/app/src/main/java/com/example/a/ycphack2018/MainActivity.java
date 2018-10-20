8e16cd6b9f8903a9a4322dcf103aea7013c9e145
bradyachterberg <achterberg@susqu.edu>
2018-10-20 17:05:40 -0400

    irrelevanttext

--- project/app/src/main/java/com/example/a/ycphack2018/MainActivity.java
+++ project/app/src/main/java/com/example/a/ycphack2018/MainActivity.java
@@ -1,3 +1,5 @@
+//links to activity_main.xml
+//launches when you run the app for the first time; invites you to make/sign in 2 ur account
 package com.example.a.ycphack2018;
 
 import android.content.Intent;
@@ -15,13 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void onLogin(View v)
-    {  setContentView(R.layout.login);
+    public void onLogin(View v) //called when user presses "login" button
+    {   Intent loginIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginIntent);
+        //go to LoginActivity, which has you sign in
     }
-    public void onJoin(View v)
-    {   setContentView(R.layout.accounttype);
-    }
-    public void onSelectCreatorAccount(View v)
-    {   setContentView(R.layout.createaccount);
-    }
+    public void onJoin(View v)  //called when user presses "newaccount" button
+    {   Intent joinIntent = new Intent(this, AccountTypeActivity.class);
+        startActivity(joinIntent);
+    }   //go to AccountTypeActivity, which asks whether you want a creator or funder account
 }
