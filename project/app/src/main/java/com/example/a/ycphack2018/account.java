6ac53537d8091861948dea6ef64055c4cf6ffee0
bradyachterberg <achterberg@susqu.edu>
2018-10-20 00:37:22 -0400

    ewgohgwjjbigwkjgj

--- project/app/src/main/java/com/example/a/ycphack2018/account.java
+++ project/app/src/main/java/com/example/a/ycphack2018/account.java
@@ -1,5 +1,20 @@
 package com.example.a.ycphack2018;
 
-public class account {
+import android.media.Image;
 
+import java.lang.reflect.Array;
+
+enum school {susky, york, umd}
+enum tag {webdev, business, science, SRE, HCI}
+
+public class account {
+    private String name;
+    private int[] rating;
+    private String desc;
+    private Array listings;
+    private Array listings_past;
+    private boolean is_on_team;
+    private Image profilepic;
+    private school user_school;
+    private tag user_tag;
 }
