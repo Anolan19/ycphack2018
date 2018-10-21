f514c4a8812007f08b60d5690e12b32be232b6c2
bradyachterberg <achterberg@susqu.edu>
2018-10-21 09:49:09 -0400

    whatevertextyouwant

--- project/app/src/main/java/com/example/a/ycphack2018/EditProfileActivity.java
+++ project/app/src/main/java/com/example/a/ycphack2018/EditProfileActivity.java
@@ -8,7 +8,7 @@ public class EditProfileActivity extends AppCompatActivity {
 
     int emailString;
     DBHelper db;
-    account userAccount;
+    //account userAccount;
     EditText displayName;
     EditText userbio;
 
@@ -18,12 +18,12 @@ public class EditProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         emailString = getIntent().getExtras().getInt("email");
         setContentView(R.layout.editprofile);
-
+/*
         displayName = findViewById(R.id.displayname);
         userbio = findViewById(R.id.bio);
         db = new DBHelper(this,null,null,1);
-        userAccount = db.getAccount(emailString);
+        account userAccount = db.getAccount(emailString);
         displayName.setText(userAccount.getName());
         userbio.setText(userAccount.getBio());
-    }
+*/  }
 }
