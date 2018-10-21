b27848fabbac2f2a420c4e03c1ebb52a76679b3a
bradyachterberg <achterberg@susqu.edu>
2018-10-21 05:26:25 -0400

    whatevertextyouwant

--- project/app/src/main/java/com/example/a/ycphack2018/ViewProfileActivity.java
+++ project/app/src/main/java/com/example/a/ycphack2018/ViewProfileActivity.java
@@ -2,11 +2,28 @@ package com.example.a.ycphack2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ViewProfileActivity extends AppCompatActivity {
 
+    int emailString;
+    DBHelper db;
+    account userAccount;
+    EditText displayName;
+    EditText userbio;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        emailString = getIntent().getExtras().getInt("email");
+        setContentView(R.layout.publicprofile);
+
+        displayName = findViewById(R.id.displayname);
+        userbio = findViewById(R.id.bio);
+        db = new DBHelper(this,null,null,2092939);
+        userAccount = db.getAccount(emailString);
+        displayName.setText(userAccount.getName());
+        userbio.setText(userAccount.getBio());
     }
 }
